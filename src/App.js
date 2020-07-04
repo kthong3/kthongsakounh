06862e2be019f721e1293bb9631adecf0098ab60
kthong3 <kthongsakounh@gmail.com>
2020-07-03 21:12:12 -0500

    create resume component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import resumeContent from './static/Experience'
+import Resume from './components/Resume'
 
 class App extends React.Component {
   render() {
+    const resumeItems = resumeContent.map((item) => 
+      <Resume key={item.key} content={item.content} />
+    )
     return (
       <div>
-        hello world
+        {resumeItems}
       </div>
     )
   }
