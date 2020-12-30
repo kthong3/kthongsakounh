898d34e9997bd3301a44ab046e55abd565fd8f18
kthong3 <kthongsakounh@gmail.com>
2020-12-30 16:32:17 -0600

    Updates

---
+++ precache-manifest.6ec9df8df2f53fb764ba21b22bf298d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f46961db6586f393259ee408e8e49e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3373f7288dd9bad065b",
+    "url": "/static/css/main.5fa23289.chunk.css"
+  },
+  {
+    "revision": "b20f004aa08f943580cc",
+    "url": "/static/js/2.5daa1d35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5daa1d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3373f7288dd9bad065b",
+    "url": "/static/js/main.50316664.chunk.js"
+  },
+  {
+    "revision": "a7fe6f3c0d050d0148df",
+    "url": "/static/js/runtime-main.e6560652.js"
+  },
+  {
+    "revision": "77f530eabbc53d4648e1f31dfdbb7b2d",
+    "url": "/static/media/jesse-schoff-Jur3IpOce6E-unsplash.77f530ea.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kthongsakounh/precache-manifest.e79eb192c50cafb38ec57d39fc5d7bf4.js"
+  "/precache-manifest.6ec9df8df2f53fb764ba21b22bf298d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kthongsakounh/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
